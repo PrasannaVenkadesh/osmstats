c475aae014d217b434d08233be7cab6e548cea4e
Prasanna Venkadesh <prasanna.s@altencalsoftlabs.com>
2017-12-20 20:22:54 +0530

    count for amenities

--- osmstats/stats.py
+++ osmstats/stats.py
@@ -8,6 +8,7 @@ class OSMStats(object):
         self.osm_file = osm_file
         self.xml_data = None
         self.contributors = {}
+        self.amenities = {}
         
 
     def get_contributors(self):
@@ -21,9 +22,21 @@ class OSMStats(object):
                     self.contributors[element.attrib['user']] = 1
         return self.contributors
 
+    def count_amenities(self, name):
+        self.amenities[name] = len(self.xml_data.xpath(f"//tag[@v='{name}']"))
+        return self.amenities[name]
+
 
 if __name__ == "__main__":
-    pondicherry = OSMStats('pondicherry.osm')
+
+    # load data from data directory
+    pondicherry = OSMStats('../data/pondicherry.osm')
+
+    # get total number of contributors
     p_contributors = pondicherry.get_contributors()
     total_contributors = len(p_contributors)
     print (f"Total Contributors: {total_contributors}")
+
+    # get total number of amenitiee
+    amenity_name = "cinema"
+    print (f"{amenity_name}: ", pondicherry.count_amenities(amenity_name))
